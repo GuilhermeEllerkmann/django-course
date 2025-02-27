535a6da9803824a990dd3e02b4f068036bd205a1
Guilherme Ellerkmann <guilherme.ellerkmannsilva@gmail.com>
2025-02-27 11:43:54 -0300

    No Comments

--- recipes/models.py
+++ recipes/models.py
@@ -1,34 +1,47 @@
 from django.contrib.auth.models import User
 from django.db import models
+from typing import Optional
+from django.core.validators import MaxLengthValidator
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=65)
+    name: models.CharField = models.CharField(max_length=65)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=65)
-    description = models.CharField(max_length=165)
-    slug = models.SlugField()
-    preparation_time = models.IntegerField()
-    preparation_time_unit = models.CharField(max_length=65)
-    servings = models.IntegerField()
-    servings_unit = models.CharField(max_length=65)
-    preparation_steps = models.TextField()
-    preparation_steps_is_html = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None
+    title: models.CharField = models.CharField(
+        max_length=65,
+        validators=[MaxLengthValidator(65)]
     )
-    author = models.ForeignKey(
+    description: models.CharField = models.CharField(max_length=165)
+    slug: models.SlugField = models.SlugField()
+    preparation_time: models.IntegerField = models.IntegerField()
+    preparation_time_unit: models.CharField = models.CharField(max_length=65)
+    servings: models.IntegerField = models.IntegerField()
+    servings_unit: models.CharField = models.CharField(max_length=65)
+    preparation_steps: models.TextField = models.TextField()
+    preparation_steps_is_html: models.BooleanField = models.BooleanField(
+        default=False
+        )
+    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
+    is_published: models.BooleanField = models.BooleanField(default=False)
+    cover: models.ImageField = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/'
+        )
+    category: Optional[models.ForeignKey] = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None
+    )
+    author: Optional[models.ForeignKey] = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -27,9 +27,8 @@ class RecipeViewsTest(RecipeTestBase):
 
     def test_recipe_home_template_loads_recipes(self):
         # Need a recipe for this test
-        self.make_recipe(category_data={
-            'name': 'Café da manhã'
-        })
+        self.make_recipe()
+
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
@@ -38,7 +37,20 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIn('Recipe Title', content)
         self.assertEqual(len(response_context_recipes), 1)
 
-        pass
+    def test_recipe_home_template_dont_load_recipes_recipes_not_published(
+            self
+            ):
+        """test if recipe is_published is false, if false dont show"""
+        # Need a recipe for this test
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        # Check if one recipe exists
+        self.assertIn(
+            'No recipes found here',
+            response.content.decode('utf-8')
+        )
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
@@ -52,6 +64,31 @@ class RecipeViewsTest(RecipeTestBase):
         )
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'This is a category test'
+
+        # Need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+
+        # Check if one recipe exists
+        self.assertIn(needed_title, content)
+
+    def test_recipe_category_template_dont_load_recipes_recipes_not_published(
+            self
+            ):
+        """test if recipe is_published is false, if false dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.category.id})
+        )
+
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
             reverse('recipes:recipe', kwargs={'id': 1})
@@ -62,4 +99,34 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(
             reverse('recipes:recipe', kwargs={'id': 1000})
         )
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_loads_the_correct_recipe(self):
+        needed_title = 'This is a detail page - It loads one recipe'
+
+        # Need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:recipe', kwargs={
+            'id': 1
+        }))
+        content = response.content.decode('utf-8')
+
+        # Check if one recipe exists
+        self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_dont_load_recipes_recipe_not_published(
+            self
+            ):
+        """test if recipe is_published is false, if false dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse(
+                'recipes:recipe',
+                kwargs={'id': recipe.id}
+            )
+        )
+
         self.assertEqual(response.status_code, 404)
