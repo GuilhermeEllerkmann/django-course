cab84b12e5238cd19adb2666a1f132403eb59365
Guilherme Ellerkmann <guilherme.ellerkmannsilva@gmail.com>
2025-03-06 23:02:18 -0300

    Section 10: Using request.GET in Django and an introduction to TDD (Test Driven Development).
     I took a few more classes, and we're now configuring the page's search function and running some tests.  We did it using TDD and MTV.  I had to stop a little earlier today to complete some college assignments.

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,8 +7,8 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home
+    path('recipes/search/', views.search, name='search'),  # Search
     path('recipes/category/<int:category_id>/',
          views.category, name='category'),  # Recipe
     path('recipes/<int:id>/', views.recipe, name='recipe'),  # Recipe
-    path('recipes/search/', lambda request: ..., name='search'),  # Search
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from recipes.models import Recipe
+from django.http import Http404
 
 
 def home(request):
@@ -33,3 +34,12 @@ def recipe(request, id):
         'recipe': recipe,
         'is_detail_page': True,
     })
+
+
+def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
+
+    return render(request, 'recipes/pages/search.html')
