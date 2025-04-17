a1eea2ff368e751c78b0c2a04a34644d9bedae80
Guilherme Ellerkmann <guilherme.ellerkmannsilva@gmail.com>
2025-04-16 22:43:02 -0300

    No comments

--- authors/forms.py
+++ authors/forms.py
@@ -37,6 +37,22 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['password'], 'Type your password')
         add_placeholder(self.fields['password2'], 'Repeat your password')
 
+    first_name = forms.CharField(
+        error_messages={'required': 'Write your first name'},
+        label='First name',
+    )
+
+    last_name = forms.CharField(
+        error_messages={'required': 'Write your last name'},
+        label='Last name',
+    )
+
+    email = forms.EmailField(
+        error_messages={'required': 'E-mail is required'},
+        label='E-mail',
+        help_text='The e-mail must be valid.'
+    )
+
     password = forms.CharField(
         required=True,
         widget=forms.PasswordInput(),
@@ -54,7 +70,10 @@ class RegisterForm(forms.ModelForm):
     password2 = forms.CharField(
         required=True,
         widget=forms.PasswordInput(),
-        label='Password2'
+        label='Password2',
+        error_messages={
+            'required': 'Please repeat your password'
+        }
     )
 
     class Meta:
@@ -68,12 +87,6 @@ class RegisterForm(forms.ModelForm):
         ]
         labels = {
             'username': 'Username',
-            'first_name': 'First name',
-            'last_name': 'Last name',
-            'email': 'E-mail',
-        }
-        help_texts = {
-            'email': 'The e-mail must be valid.',
         }
         error_messages = {
             'username': {
